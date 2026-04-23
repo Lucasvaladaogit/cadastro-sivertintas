fdcfa98ac537cf6449ea3097abc4b17756d5bde8
Lucasvaladaogit <28891@raphaeldisanto.com.br>
2026-04-23 12:14:56 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Logo from './assets/LOGO.png'
+import logo from "./assets/logo.jpg"
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <body>
-      
-    </body>
-      <div className="auth-container">
-        <div className="auth-background">
-          <div className="gradient-orb orb-1"></div>
-          <div className="gradient-orb orb-2"></div>
-          <div className="gradient-orb orb-3"></div>
+  <body>
+    <header>
+      <div id="logo"><img src={logo} alt="" /></div>
+    </header>
+      <div className="right">
+        <div className="login-box">
+          <h2>Fazer Cadastro</h2>
+
+          <div className="input-group">
+            <input type="text" placeholder="Nome" />
+          </div>
+
+            <div className="input-group">
+            <input type="text" placeholder="Sobrenome" />
+          </div>
+
+          <div className="input-group">
+            <input type="password" placeholder="CPF" />
+          </div>
+
+           <div className="input-group">
+            <input type="text" placeholder="E-MAIL" />
+          </div>
+
+
+           <div className="input-group">
+            <input type="password" placeholder="Telefone" />
+          </div>
+
+            <div className="input-group">
+            <input type="password" placeholder="Crie sua Senha..." />
+          </div>
+          <button>Entrar</button>
+
+          <a href="#" className="forgot">
+            
+          </a>
         </div>
-
-        <div className="auth-card">
-          <div className="auth-header">
-              <img src={Logo} alt="" id='logoSilver'/>
-            <h1>Bem-Vindo</h1>
-            <p>Cadastre-se para continuar</p>
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="usuario">Usuário</label>
-            <input 
-              type="text" 
-              id='usuario' 
-              name='usuario' 
-              placeholder='Crie nome de usuario' 
-              required  />
-          </div>
-          
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input 
-              type="text" 
-              id='email' 
-              name='email' 
-              placeholder='Digite seu email' 
-              required  />
-          </div>
-          <div className="form-group">
-            <label htmlFor="Telefone">Digite seu Telefone</label>
-            <input 
-              type="Telefone" 
-              id='Telefone' 
-              name='telefone' 
-              placeholder='(00) 1234-5678' 
-              required  />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="senha">Crie uma Senha</label>
-            <input 
-              type="password" 
-              id='senha' 
-              name=' senha' 
-              placeholder='*********' 
-              required  />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="confirmarSenha">Confirmar Senha</label>
-            <input 
-              type="password" 
-              id='confirmarSenha' 
-              name='confirmarSenha' 
-              placeholder='*********' 
-              required  />
-          </div>
-
-
-          <button type='submit' className='submit-btn'>
-            Cadastrar
-          </button>
-
-          <div className="auth-footer">
-            <p>Já tem uma conta?</p>
-            <button className='toggle-btn'>Faça login</button>
-          </div>
-
-        </div>
-
       </div>
-    </>
-  )
+    </body>
+  );
 }
 
-export default App
+export default App;
